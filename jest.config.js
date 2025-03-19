27f74c13500dedb4bd20fdd953eee1ff1ecaf53b
Dr. Ernie Prabhakar <19791+drernie@users.noreply.github.com>
2025-03-18 18:55:30 -0700

    Revert "fix: update jest config for ES modules support"
    This reverts commit 8e874dcf48c4701543839ec52696d1347093f4dd.

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,8 @@
-/** @type {import('jest').Config} */
-export default {
-    testEnvironment: "node",
-    roots: ["<rootDir>/test"],
-    testMatch: ["**/*.test.ts"],
-    transform: {
-        "^.+\\.tsx?$": "ts-jest",
-    },
-    extensionsToTreatAsEsm: [".ts"],
-    moduleNameMapper: {
-        "^(\\.{1,2}/.*)\\.js$": "$1",
-    },
+module.exports = {
+  testEnvironment: 'node',
+  roots: ['<rootDir>/test'],
+  testMatch: ['**/*.test.ts'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  }
 };
