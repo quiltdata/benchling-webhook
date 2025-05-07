ec3fa9f865e46ce2973a92a884f0494abd368dc4
Dr. Ernie Prabhakar <19791+drernie@users.noreply.github.com>
2025-05-07 15:10:39 -0700

    formatFields

--- lib/templates/readme.ts
+++ lib/templates/readme.ts
@@ -59,9 +59,39 @@ export class ReadmeTemplate extends BaseTemplate {
         );
     }
 
+    private formatFields(): stepfunctions.Map {
+        const appendFormattedField = new stepfunctions.Pass(this.scope, 'AppendFormattedField', {
+            parameters: {
+                "formattedField.$": "States.Format('* {}: {}', $.name, $.displayValue)"
+            },
+            resultPath: "$.formattedField"
+        });
+
+        return new stepfunctions.Map(this.scope, 'FormatFields', {
+            itemsPath: '$.entry.fields',
+            resultPath: '$.fieldsFormatted',
+        }).itemProcessor(appendFormattedField);
+    }
+
+    private formatCustomFields(): stepfunctions.Map {
+        const appendFormattedCustomField = new stepfunctions.Pass(this.scope, 'AppendFormattedCustomField', {
+            parameters: {
+                "formattedCustomField.$": "States.Format('* {}: {}', $.name, $.value)"
+            },
+            resultPath: "$.formattedCustomField"
+        });
+
+        return new stepfunctions.Map(this.scope, 'FormatCustomFields', {
+            itemsPath: '$.entry.customFields',
+            resultPath: '$.customFieldsFormatted',
+        }).itemProcessor(appendFormattedCustomField);
+    }
+
     public createMarkdown(): stepfunctions.Chain {
         return stepfunctions.Chain
             .start(this.setupFiles())
+            .next(this.formatFields())
+            .next(this.formatCustomFields())
             .next(super.createMarkdown());
     }
 }
